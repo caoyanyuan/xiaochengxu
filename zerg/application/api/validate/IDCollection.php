725f87644600904c6875185a61a215e709572457
cindy <1689456277@qq.com>
2018-07-16 17:57:15 +0800

    ids验证器：验证1,2,3,$message定义报错信息，注意rule的值：require|checkIDs不要随意加空格

--- zerg/application/api/validate/IDCollection.php
+++ zerg/application/api/validate/IDCollection.php
@@ -12,18 +12,25 @@ namespace app\api\validate;
 class IDCollection extends BaseValidate
 {
     protected $rule = [
-       'ids' => 'require | checkIDs'
+       'ids' => 'require|checkIDs'
     ];
 
-    protected function checkIds($value)
+    protected $message = [
+        'ids' => 'ids参数必须为以逗号分隔的多个正整数'
+    ];
+
+    protected function checkIDs($value)
     {
-        $values = explode(',',$value);
-        if(empty($values)){
+        $values = explode(',', $value);
+        if (empty($values)) {
             return false;
         }
-        foreach($values as $id){
-
+        foreach ($values as $id) {
+            if (!$this->isPositiveIntegar($id)) {
+                return false;
+            }
         }
+        return true;
     }
 
 }
