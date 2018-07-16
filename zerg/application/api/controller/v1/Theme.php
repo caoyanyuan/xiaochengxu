41cb324826cfbaba384d7fc1aec15b6e4e6f47ae
cindy <1689456277@qq.com>
2018-07-16 17:53:23 +0800

    Theme控制器：调用模型方法返回需要数据。getSimpleList中直接查询数据库：因为查询比较简单。可以直接放在控制器

--- zerg/application/api/controller/v1/Theme.php
+++ zerg/application/api/controller/v1/Theme.php
@@ -10,11 +10,28 @@ namespace app\api\controller\v1;
 
 
 use app\api\validate\IDCollection;
+use app\api\model\Theme as ThemeModel;
+use app\api\validate\IdPositiveInt;
 
 class Theme
 {
     public function getSimpleList($ids = '')
     {
         (new IDCollection()) -> goCheck();
+        $ids = explode(',', $ids);
+        $result = ThemeModel::with('topicImg,headImg')->select($ids);
+
+        if (count($result) < 1) {
+            throw new ThemeException();
+        }
+
+        return $result;
+    }
+
+    public function getComplexOne($id)
+    {
+        (new IdPositiveInt()) -> goCheck();
+        $result = ThemeModel::getThemeWithProducts($id);
+        return $result;
     }
 }
