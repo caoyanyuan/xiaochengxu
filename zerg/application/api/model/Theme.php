1b12c888b8acf00aad8c2e85432e2b8958033ef1
cindy <1689456277@qq.com>
2018-07-16 17:47:09 +0800

    Theme模型；定义theme与image的一对一，theme与product的多对多，并提供：获得theme详情接口

--- zerg/application/api/model/Theme.php
+++ zerg/application/api/model/Theme.php
@@ -11,13 +11,26 @@ namespace app\api\model;
 
 class Theme extends  BaseModel
 {
-    public function topicImg()
-    {
-        return $this->belongsTo('Imgae','top_img_id','id');
+    public function topicImg() {
+        return $this->belongsTo('Image', 'topic_img_id', 'id');
     }
 
-    public function headImg()
-    {
-        return $this->belongsTo('Imgae','head_img_id','id');
+    public function headImg() {
+        return $this->belongsTo('Image', 'head_img_id', 'id');
     }
+
+    /**
+     * 关联product，多对多关系
+     */
+    public function products() {
+        return $this->belongsToMany(
+            'Product', 'theme_product', 'product_id', 'theme_id');
+    }
+
+    public static function getThemeWithProducts($id) {
+        $products = self::with('topicImg,headImg,products')->select($id);
+        return $products;
+    }
+
+
 }
