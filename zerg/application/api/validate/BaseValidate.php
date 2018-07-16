537a53365f2cf117d99d616b66d68f1431507dbf
cindy <1689456277@qq.com>
2018-07-16 17:54:45 +0800

    BaseValidate验证基类：发现isPositiveIntegar可以多出调用。将isPositiveIntegar放到验证基类中供公共调用

--- zerg/application/api/validate/BaseValidate.php
+++ zerg/application/api/validate/BaseValidate.php
@@ -31,4 +31,13 @@ class BaseValidate extends Validate
             return true;
         }
     }
+
+    protected function isPositiveIntegar($value, $rule="",$data="",$field="")
+    {
+        if(is_numeric($value) && is_int($value+0) && ($value+0)> 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
