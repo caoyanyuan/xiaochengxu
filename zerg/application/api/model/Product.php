74c65dcb07f2fcb023cca2b9f7ce87a4d70fd9df
cindy <1689456277@qq.com>
2018-07-16 17:50:44 +0800

    Product模型：定义隐藏属性，提供取main_img_url的读取器，用以补全url。通过数据冗余：减少从product里面查image表的查询

--- zerg/application/api/model/Product.php
+++ zerg/application/api/model/Product.php
@@ -11,5 +11,10 @@ namespace app\api\model;
 
 class Product extends BaseModel
 {
+    public $hidden = ['delete_time', 'main_img_id', 'pivot', 'from', 'category_id',
+        'create_time', 'update_time'];
 
+    protected function getMainImgUrlAttr($value,$data){
+        return $this->prefixImgUrl($value, $data);
+    }
 }
