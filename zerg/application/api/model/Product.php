9055d977da58485c8b72205c0ccf8430f755584c
cindy <1689456277@qq.com>
2018-07-19 17:05:07 +0800

    产品模型：recent新品方法，拿分类产品方法

--- zerg/application/api/model/Product.php
+++ zerg/application/api/model/Product.php
@@ -11,10 +11,24 @@ namespace app\api\model;
 
 class Product extends BaseModel
 {
-    public $hidden = ['delete_time', 'main_img_id', 'pivot', 'from', 'category_id',
+    protected $hidden = ['delete_time', 'main_img_id', 'pivot', 'from', 'category_id',
         'create_time', 'update_time'];
 
     protected function getMainImgUrlAttr($value,$data){
         return $this->prefixImgUrl($value, $data);
+    }
+
+    //create_time:排序字段。desc：降序
+    public static function getRecent($count){
+        $products = self::limit($count)
+            ->order('create_time desc')
+            ->select();
+        return $products;
+    }
+
+    public static function getProductByCategoryId($CategoryId){
+        $products = self::where('category_id','=',$CategoryId)
+            ->select();
+        return $products;
     }
 }
