a8ee15ea4242ae506ba6a85fc5442505d0ba4814
cindy <1689456277@qq.com>
2018-07-19 17:22:54 +0800

    产品模型：recent方法获取最近新品，getAllInCategory获取分类产品

---
+++ zerg/application/api/controller/v1/Product.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/16
+ * Time: 18:08
+ */
+
+namespace app\api\controller\v1;
+
+use app\api\validate\Count;
+use app\api\Model\Product as ProductModel;
+use app\api\validate\IdPositiveInt;
+use app\lib\exception\ProductException;
+
+
+class Product
+{
+    public function getRecent($count=15)
+    {
+        (new Count()) -> goCheck();
+        $products = ProductModel::getRecent($count);
+        if($products ->isEmpty()){
+            throw new ProductException();
+        }
+        $products = $products->hidden(['summary']);
+        return $products;
+    }
+
+    public function getAllInCategory($id){
+        (new IdPositiveInt()) -> goCheck();
+        $products = ProductModel::getProductByCategoryId($id);
+        if($products ->isEmpty()){
+            throw new ProductException();
+        }
+        $products = $products->hidden(['summary']);
+        return $products;
+    }
+}
