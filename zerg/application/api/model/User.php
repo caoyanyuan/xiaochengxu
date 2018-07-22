5824d272fb8912ceb2db886c4872d67202bcf54a
cyy <1689456277@qq.com>
2018-07-22 23:23:20 +0800

    user模型：关联userAddress表

--- zerg/application/api/model/User.php
+++ zerg/application/api/model/User.php
@@ -11,10 +11,14 @@ namespace app\api\model;
 
 class User extends BaseModel
 {
-    public static function getByOpenid($openid)
-    {
+    public function address(){
+        return $this->hasOne('UserAddress','user_id','id');
+    }
+
+    public static function getByOpenid($openid){
         $user = User::where('openid','=',$openid)
             ->find();
         return $user;
     }
+
 }
