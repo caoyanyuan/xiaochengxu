3a41d36c255371d82689cbf0cd569660e5549e36
cyy <1689456277@qq.com>
2018-07-29 15:36:48 +0800

    增加发送post的http请求公共函数

--- zerg/application/common.php
+++ zerg/application/common.php
@@ -25,6 +25,28 @@ function curl_get($url, &$httpCode = 0)
     return $file_contents;
 }
 
+function curl_post_raw($url, $rawData)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $rawData);
+    curl_setopt(
+        $ch, CURLOPT_HTTPHEADER,
+        array(
+            'Content-Type: text'
+        )
+    );
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return ($data);
+}
+
+
 function getRandChar($length)
 {
     $str = null;
@@ -39,3 +61,4 @@ function getRandChar($length)
 
     return $str;
 }
+
