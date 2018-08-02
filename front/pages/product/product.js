42e54c5521ff0351817313c835bc472a1e40a28c
cindy <1689456277@qq.com>
2018-08-02 18:34:38 +0800

    产品详情页：加入购物车动画，调用cart-model里面拿总数方法

--- front/pages/product/product.js
+++ front/pages/product/product.js
@@ -1,66 +1,103 @@
 // pages/product/product.js
+import {Product} from 'product-model.js';
+import {Cart} from '../cart/cart-model.js'
+var product = new Product();
+var cart = new Cart();
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    id: '',  //当前产品id
+    countsArray: [1,2,3,4],   //选择数量下拉列表
+    countSelected: 1,          //当前选择数量
+    currentTabsIndex: 0,      //选项卡当前index
+    productInfo: "",           //当前产品详情
+    cartTotalCounts: 0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+ 
   onLoad: function (options) {
-    console.log(options);
+    this.data.id = options.id;
+    this._load();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  _load: function (){
+    product.getProduct(this.data.id,(res)=>{
+      this.setData({
+        'cartTotalCounts': cart.getCartTotalCounts().counts1,
+        'product':res
+      });
+      this.data.productInfo = res;
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  //数量选择
+  bindPickerChange: function (event){
+    var _index = event.detail.value;
+    this.setData({
+      countSelected: this.data.countsArray[_index]
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  //tab切换
+  onTabsItemTap: function(event){
+    var index = product.getDataSet(event,'index');
+    this.setData({
+      currentTabsIndex: index
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+  onAddingToCartTap: function(events){
+    if(this.data.isFly){
+      return;
+    }
+    this._flyToCartEffect(events)
+    this.addToCart();
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
+  addToCart: function(){
+    var item = {},
+        arr = ['id','name','price','main_img_url'];
+    for(var key in this.data.productInfo){
+      
+      if (arr.indexOf(key)>-1){
+        item[key] = this.data.productInfo[key];
+      }
+    }
+    cart.add(item,this.data.countSelected);
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
+  _flyToCartEffect:function(events){
+    //获得当前点击的位置，距离可视区域左上角
+    var touches=events.touches[0];
+    var diff={
+          x:'25px',
+          y:25-touches.clientY+'px'
+        },
+        style='display: block;-webkit-transform:translate('+diff.x+','+diff.y+') rotate(350deg) scale(0)';  //移动距离
+    this.setData({
+      isFly:true,
+      translateStyle:style
+    });
+    setTimeout(()=>{
+      this.setData({
+        isFly:false,
+        translateStyle:'-webkit-transform: none;',  //恢复到最初状态
+        isShake:true,
+      });
+      setTimeout(()=>{
+        var counts = this.data.cartTotalCounts + this.data.countSelected;
+        this.setData({
+          isShake:false,
+          cartTotalCounts:counts
+        });
+      },200);
+    },1000);
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  onCartTap:function(){
+    wx.switchTab({
+      url:'/pages/cart/cart'
+    })
   }
 })
