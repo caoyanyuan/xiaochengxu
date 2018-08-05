6425d2285de0f10fb1c00addd52c3e1af99229b9
cyy <1689456277@qq.com>
2018-08-05 23:53:08 +0800

    重构Base下的Request方法：加上接口因为授权方面的报错的统一处理：401的时候再拿一次token：AOP体现

--- front/utils/base.js
+++ front/utils/base.js
@@ -1,27 +1,53 @@
 import {Config} from 'config.js'
+import {Token} from 'token.js'
+
 class Base{
     constructor(){
         this.baseRequestUrl = Config.baseUrl;
     }
 
-    request(params){
+    /*
+      noRefetch: 授权失败的401只重试一次拿token，加入标志位以防止无数次调用request函数
+    */
+    request(params, noRefetch){
+        var that = this;
         var url = this.baseRequestUrl + params.url;
         var method = params.type ? params.type : 'GET';
         wx.request({
-            url:url,
-            type:method,
-            data:params.data,
+            url: url,
+            method: method,
+            data: params.data,
             header: {
                 'content-type':'application/json',
                 'token':wx.getStorageSync('token')
             },
-            success:function (res) {
-              params.sCallback && params.sCallback(res.data);
+            success: function (res) {
+              var code = res.statusCode.toString(),
+                  startChar = code.charAt(0);
+              if(startChar == 2){
+                params.sCallback && params.sCallback(res.data);
+              }else{
+                if (code == 401 && !noRefetch){
+                  that._reFetch(params);
+                }
+                if(noRefetch){ //重试之后依然报错，则传出报错函数
+                  params.eCallback && params.eCallback(res.data);
+                }
+              }
             },
-            error:function (err) {
+            error: function (err) {
               
             }
         })
+    }
+
+    // 当“接口”出现401错误时候。再去拿一次token。然后再调用一次“接口”
+    _reFetch(params){
+      var token = new Token();
+      token.getTokenFromServer((res) => {
+        this.request(params, true);
+      });
+      
     }
 
     getDataSet(event,key){
