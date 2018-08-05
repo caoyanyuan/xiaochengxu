d778cfc0b702bd98414195edfb30526e94257c35
cyy <1689456277@qq.com>
2018-08-05 23:59:01 +0800

    新增接口路由

--- zerg/application/route.php
+++ zerg/application/route.php
@@ -15,7 +15,7 @@ use think\Route;
 Route::get('api/:version/banner/:id','api/:version.Banner/getBanner'); //三段式 模块 / 控制器 / 方法名
 
 //theme
-Route::get('api/:version/theme','api/:version.Theme/getSimpleList');
+Route::get('api/:version/themes','api/:version.Theme/getSimpleList');
 Route::get('api/:version/theme/:id','api/:version.Theme/getComplexOne');
 
 /*Route::get('api/:version/product/recent','api/:version.Product/getRecent');
@@ -26,7 +26,7 @@ Route::get('api/:version/product/:id','api/:version.Product/getOne');*/
 Route::group('api/:version/product',function(){
     Route::get('/recent','api/:version.Product/getRecent');
     Route::get('/by_category','api/:version.Product/getAllInCategory');
-    Route::get('/:id','api/:verison.Product/getOne');
+    Route::get('/:id','api/:version.Product/getOne');
 });
 
 //category
@@ -34,10 +34,12 @@ Route::get('api/:version/category/all','api/:version.Category/getAllCategories')
 
 //login
 Route::post('api/:version/token/user','api/:version.Token/getToken');
+Route::post('api/:version/token/verify','api/:version.Token/verify');
+
 
 //Address
 Route::post('api/:verison/address','api/:verison.Address/createOrUpdate');
-
+Route::get('api/:verison/address','api/:verison.Address/getAddress');
 
 //Order
 Route::post('api/:verison/order','api/:verison.Order/placeOrder');
