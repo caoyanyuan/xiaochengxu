d9b644e9a9ebcbbbe76cd90c538069e6778b7351
cindy <1689456277@qq.com>
2018-08-06 14:13:38 +0800

    支付结果页

---
+++ front/pages/pay-result/pay-result.js
@@ -0,0 +1,21 @@
+// pages/pay-result/pay-result.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  onLoad: function(options){
+    
+  },
+
+  viewOrder: function(){
+    wx.navigateBack({
+      delta:1
+    })
+  }
+  
+})
